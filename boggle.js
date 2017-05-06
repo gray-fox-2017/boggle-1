d53cd3ce8c5d7ceef2a5d874bec4afdf8feef8d5
ingelieur <ingelieur@gmail.com>
2017-05-06 17:02:56 +0700

    boggle board done

---
+++ boggle.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class BoggleBoard {
+  constructor() {
+  }
+
+  shake(n) {
+    let board = [];
+    for (let i=0; i<n; i++) {
+      board [i] = [];
+      for (let j=0; j<n; j++) {
+        board[i][j] = String.fromCharCode(Math.floor(Math.random()*26+65));
+      }
+    }
+    let finalBoard = this.board(board);
+    return finalBoard;
+  }
+
+  board(board) {
+    let printBoard = "";
+    let dashLine = "-----------------"
+    printBoard += dashLine+"\n";
+    for (let i=0; i<board.length;i++){
+      printBoard += "| ";
+      printBoard += board[i].join(" | ") + " |" + "\n";
+      printBoard += dashLine + "\n";
+    }
+    return printBoard;
+  }
+}
+
+let boggle = new BoggleBoard();
+console.log(boggle.shake(4));
