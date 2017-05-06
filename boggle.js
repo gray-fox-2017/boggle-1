61517e24537ac860cc77eb65e400a925a654649d
fajarkarim <fajarabdulkarim@gmail.com>
2017-05-06 17:09:57 +0700

    boggle done

---
+++ boggle.js
@@ -0,0 +1,23 @@
+
+function generateRandomLetter() {
+  let letterArr = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+  let randomIndex = Math.floor(Math.random() * (letterArr.length - 0)) + 0;
+  return letterArr[randomIndex]
+}
+
+function displayBoard() {
+  let boardLength = 4
+  let boardArr = []
+  for (let i = 0; i < boardLength; i++) {
+    let tmpArr = []
+    for (let j = 0; j < boardLength; j++) {
+      let randomLetter = generateRandomLetter();
+      tmpArr.push(randomLetter);
+    }
+    boardArr.push(tmpArr)
+  }
+  return boardArr;
+}
+
+let displayArr = displayBoard()
+console.log(displayArr);
